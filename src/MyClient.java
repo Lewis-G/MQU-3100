8ef414708afdf5b6b45e7369a2c36f4a6aceb529
LewisG <lewisgriffith9@gmail.com>
2023-04-02 19:34:51 +1000

    Removed unnecessary sending of REDY, added new comments

--- src/MyClient.java
+++ src/MyClient.java
@@ -13,100 +13,99 @@ public class MyClient {
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-            String tempInputString = "";
-
             // Authenticate
             dout.write(("HELO\n").getBytes());
-            tempInputString = (String) in.readLine();
-            //System.out.println("server = " + tempInputString);
-
+            in.readLine();   //ds-server sends 'OK'
             dout.write(("AUTH lewis\n").getBytes());
-            tempInputString = (String) in.readLine();
-            //System.out.println("server = " + tempInputString);
+            in.readLine();   //ds-server sends 'OK'
 
             dout.write(("REDY\n").getBytes());
-            tempInputString = (String) in.readLine(); // Client sends first job
-            //System.out.println("server = " + tempInputString);
+            String firstJOBN = (String) in.readLine();  //ds-server sends first job, stored in variable for later use
 
             dout.write(("GETS All\n").getBytes());
-            String dataN = (String) in.readLine();
-            //System.out.println("server = " + dataN);
+            String dataN = (String) in.readLine();  //ds-server sends 'DATA nRecs recLen'
+            //This is the first message in response to GETS
+            //nRecs indicates the number of subsequent messages ...
+            //This will be used as a condition of the loop
 
-            // Sort through data message
+            //Split the DATA message, and isolate nRecs
             String[] dataSplit = dataN.split(" ", 3);
             int numberOfRecords = Integer.valueOf(dataSplit[1]);
 
             // Respond with OK to recieve GETS records
             dout.write(("OK\n").getBytes());
 
-            String tempServerType;
-            String biggestServerType = "";
-            String tempRecord = "";
-            String[] tempRecordSplit;
+            //These variables are used in the for loop
+            String tempMessage = "";    //Stores the message from ds-server, copied over by each iteration
+            String[] tempMessageSplit;  //Each server message is split into an array
 
-            int numberOfLargestServers = 1;
-            int tempNumCores = 0;
-            int greatestNumCores = 0;
+            int tempNumCores = 0;   //Stores the number of cores, taken from ds-server message
+            int greatestNumCores = -1;  //Stores the greatest number of cores at any point of the loop
+
+            String tempServerType;   //Stores ds-server type, taken from ds-server message
+            String biggestServerType = "";  //Stores the name of first known largest server at any point of the loop
+
+            int numberOfLargestServers = 1; //Stores the number of servers that belong to the largest server type
 
             for (int i = 0; i < numberOfRecords; i++) {
 
-                tempRecord = (String) in.readLine();
-                tempRecordSplit = tempRecord.split(" ", 8);
+                tempMessage = (String) in.readLine();
+                tempMessageSplit = tempMessage.split(" ", 8);
 
-                tempServerType = tempRecordSplit[0];
-                tempNumCores = Integer.parseInt(tempRecordSplit[4]);
+                tempServerType = tempMessageSplit[0];
+                tempNumCores = Integer.parseInt(tempMessageSplit[4]);
 
                 if (tempNumCores > greatestNumCores) {
 
+                    //A larger server type has been identifed
                     biggestServerType = tempServerType;
                     numberOfLargestServers = 1;
                     greatestNumCores = tempNumCores;
 
                 } else if (tempNumCores == greatestNumCores && biggestServerType.equals(tempServerType)) {
+                    
+                    //A subsequent server of the largest type has been identified
                     numberOfLargestServers++;
                 }
             }
 
             // After recieving GETS records
             dout.write(("OK\n").getBytes());
-            tempInputString = (String) in.readLine();
-            //System.out.println("server = " + tempInputString);
-            dout.write(("REDY\n").getBytes());
+            in.readLine();
 
-            String loopMessage = (String) in.readLine();
-            String schdMessage = "";
-            //System.out.println("server = " + loopMessage);
-            int counter = 0;
-
+            String schdMessage = "";    //Stores each SCHD String before sending the command
+            int counter = 0;    //Stores the current server in the queue
+            String loopMessage = firstJOBN; //Stores JOBN, JCPL, and NONE messages sent by ds-server
+            
+            //while loop conditions
             boolean loopMessageIsJOBN = loopMessage.substring(0, 4).equals("JOBN");
             boolean loopMessageIsJCPL = loopMessage.substring(0, 4).equals("JCPL");
 
             while (loopMessageIsJOBN || loopMessageIsJCPL) {
 
-                //System.out.println("(Loop) Server message is " + loopMessage + "\n");
-
                 if (loopMessageIsJCPL) {
 
+                    //Job is completed
                     dout.write(("REDY\n").getBytes());
                     loopMessage = (String) in.readLine();
 
                 } else {
 
+                    //Calls a static function, returns jobID
                     int currentJobID = ParseJOBN(loopMessage);
 
                     schdMessage = "SCHD " + currentJobID + " " + biggestServerType + " " + counter + "\n";
-                    //System.out.println("(Loop) Client message is " + schdMessage);
                     dout.write((schdMessage).getBytes());
 
-                    loopMessage = (String) in.readLine(); // server sends message saying that the job is beign scheduled
+                    loopMessage = (String) in.readLine(); //ds-server confirms that the job is beign scheduled
 
                     dout.write(("REDY\n").getBytes());
-                    loopMessage = (String) in.readLine(); // If there a more jobs, then JOBN is sent
+                    loopMessage = (String) in.readLine(); //If there a more jobs, then JOBN is sent
 
                     counter++;
+                    //ensures that SCHD is sent to non-existant server
                     if (counter == numberOfLargestServers) {
                         counter = 0;
-                        // loop of boolean values, after schd bool = false, after jcpl bool = true
                     }
 
                 }//end of else block
@@ -117,12 +116,9 @@ public class MyClient {
 
             } // end of while loop
 
-            //System.out.println("(After last loop) Server message is " + loopMessage);
-
+            //After NONE is sent, end communication and close the connection
             dout.write(("QUIT\n").getBytes());
-            tempInputString = (String) in.readLine();
-            //System.out.println("server = " + tempInputString);
-
+            in.readLine();
             dout.flush();
             dout.close();
             s.close();
@@ -142,5 +138,4 @@ public class MyClient {
 
         return Integer.parseInt(jobID);
     }
-
 }
